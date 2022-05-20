7669dab3b07f7d3289d39e28409389f662c47c39
Charles Wilis <charles.wilis@gmail.com>
2022-05-20 17:17:12 -0300

    alterando main.js para apontar para host da produção

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-
+//Desenv:
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+//Prod:
+axios.defaults.baseURL = 'https://wilis.pythonanywhere.com'
 
 
 createApp(App).use(store).use(router,axios).mount('#app')
