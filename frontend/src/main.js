8df638a5bda45f4c2adf964f47246342a683f5f0
Charles Wilis <charles.wilis@gmail.com>
2022-05-20 17:31:00 -0300

    ajustando static novamente

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,10 @@ import store from './store'
 import axios from 'axios'
 
 //Desenv:
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+//axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 //Prod:
-//axios.defaults.baseURL = 'https://wilis.pythonanywhere.com'
+axios.defaults.baseURL = 'https://wilis.pythonanywhere.com'
 
 
 createApp(App).use(store).use(router,axios).mount('#app')
