a5502d6684b352270b2e7967a0f6e49efa2be16e
Charles Wilis <charles.wilis@gmail.com>
2022-05-23 14:53:32 -0300

    Melhorias na interface de instalações, para adaptar ao celular

--- frontend/src/views/Cliente.vue
+++ frontend/src/views/Cliente.vue
@@ -3,21 +3,22 @@
       <div class="notification is-danger" v-if="errors.length">
         <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
       </div>
+      <form @submit.prevent="gravar_ou_criar">
       <table class="table">
         <tbody>
-          <tr> <td>CPF do Cliente</td> <td> <input v-model="cliente.cpf_cliente" type="number" max="99999999999" style="width: 11em" size="11"></td></tr>
-          <tr> <td>Nome da Instalação</td> <td> <input v-model="cliente.nome" type="text" max="40"> </td></tr>
+          <tr> <td>CPF do Cliente</td> <td> <input v-model="cliente.cpf_cliente" required type="number" max="99999999999" style="width: 11em" size="11"></td></tr>
+          <tr> <td>Nome da Instalação</td> <td> <input v-model="cliente.nome" required type="text" max="40"> </td></tr>
           <tr> <td>Endereço</td> <td> <input v-model="cliente.endereco" type="text" max="84"> </td></tr>
           <tr> <td>E-mail</td> <td> <input v-model="cliente.email" type="email" > </td></tr>
           <tr> <td>Telefone</td> <td> <input v-model="cliente.telefone" type="text" size="13" max="15"> </td></tr>
-          <tr> <td>Desconto</td> <td> <input v-model="cliente.desconto" type="number" min="0" max="100" step="1" size="3">%</td></tr>
-          <tr> <td>Bônus</td> <td> R$ <input v-model="cliente.bonus" type="number" min="0"  step="0.01"></td></tr>
-      
-          
-
+          <tr> <td>Desconto</td> <td> <input v-model="cliente.desconto" required type="number" min="0" max="100" step="1" size="3">%</td></tr>
+          <tr> <td>Bônus</td> <td> R$ <input v-model="cliente.bonus" required type="number" min="0"  step="0.01"></td></tr>
         </tbody>
       </table>
-      <button @click="gravar(cliente)">Gravar</button>
+      <button @click="$router.push('/clientes')">Voltar</button>
+      <button type="submit">Gravar</button>
+      
+      </form>
     </div>
 </template>
 
@@ -30,17 +31,9 @@ export default {
   name: 'Clientes',
   data() {
     return {
-      cliente: Object,
+      cliente: {},
       errors: [],
-      novo_cliente: {
-          cpf_cliente: Number,
-          nome: '',
-          telefone: '',
-          endereco: '', 
-          email: '',
-          desconto:20 ,
-          bonus:0 ,
-      },
+      id:this.$route.params.id,
     }
   },
   
@@ -48,7 +41,10 @@ export default {
 
   },
   mounted(){
-  },
+    if (this.id!=0){ 
+      this.cliente=this.getCliente()
+    }
+},
   methods:{
     logout() {
           toast ({
@@ -64,29 +60,38 @@ export default {
             this.$store.commit('removeToken')
             this.$router.push('/log-in')
     },
-    async validar_campos(cliente){
-      this.errors = []
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(cliente.email) ==false) {
-          this.errors.push('Preencha o e-mail corretamente')
-          return null
-      }
-      if (cliente.nome==''){
-        this.errors.push('Preencha o nome da instalação')
-      }
-      if (cliente.desconto<0 || cliente.desconto>100 || cliente.desconto==''){
-        this.errors.push('O valor de desconto deve ser entre 0% e 100%')
-      }
-      if (cliente.bonus<0 || cliente.bonus==''){
-        this.errors.push('O valor do bônus deve ser preenchido com 0 ou qualquer número positivo')
-      }
+
+    async getCliente(){
+      this.$store.commit('setIsLoading', true)
+      await axios
+          .get ("/api/v1/clientes/"+this.id+"/")
+          .then(response => {
+            this.cliente=response.data
+          })
+          .catch(error => {
+            if (error.response.status === 401) {
+                console.log('Ticket expirado. Necessário novo login')
+                this.logout()
+            }
+            console.log(error)
+          })
+      this.$store.commit('setIsLoading', false)
     },
-    async gravar(cliente){
-      this.validar_campos(cliente)
+    gravar_ou_criar(){
+      if (this.id==0){
+        this.novo()
+      } else{
+        this.gravar()
+      }
+
+    },
+    async gravar(){
+      
       if (!this.errors.length) {
         this.$store.commit('setIsLoading', true)
-        console.log(cliente)
+        console.log(this.cliente)
         await axios
-          .put ("/api/v1/clientes/"+cliente.id+"/", cliente)
+          .put ("/api/v1/clientes/"+this.id+"/", this.cliente)
           .then(response => {
             toast ({
             message: 'Cliente alterado',
@@ -96,7 +101,8 @@ export default {
             duration: 2000,
             position: 'bottom-right'
             })
-            this.getClientes()
+            this.cliente=response.data
+            this.$router.push('/clientes')
           })
           
           .catch(error => {
@@ -110,13 +116,13 @@ export default {
         this.$store.commit('setIsLoading', false)
       }
     },
-    async novo(cliente){
-      await this.validar_campos(cliente)
+    async novo(){
+
       if (!this.errors.length) {
         this.$store.commit('setIsLoading', true)
-        console.log(cliente)
+        console.log(this.cliente)
         await axios
-          .post ("/api/v1/clientes/", cliente)
+          .post ("/api/v1/clientes/", this.cliente)
           .then(response => {
             toast ({
             message: 'Cliente adicionado',
@@ -126,7 +132,8 @@ export default {
             duration: 2000,
             position: 'bottom-right'
             })
-            this.getClientes()
+            this.cliente=response.data
+            this.$router.push('/clientes')
           })
           
           .catch(error => {
@@ -136,7 +143,6 @@ export default {
               }
               console.log(error)
             })    
-        await new Promise(r => setTimeout(r, 3000));
         this.$store.commit('setIsLoading', false)
       }
     },
